1ec93be1f1d31f630a7a94e2ab1721d6801f132e
wonill <119827402+wonill@users.noreply.github.com>
2023-05-19 13:25:30 +0900

    [level 1] Title: K번째수, Time: 0.23 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+		
+		for(int i= 0; i < commands.length; i++) {
+			List<Integer> splitedList = new ArrayList<>();
+			for(int j = commands[i][0] - 1; j < commands[i][1]; j++) {
+				splitedList.add(array[j]);
+			}
+			
+			Collections.sort(splitedList);
+			answer[i] = splitedList.get(commands[i][2] - 1);
+		}
+        
+        
+        
+        return answer;
+    }
+}
