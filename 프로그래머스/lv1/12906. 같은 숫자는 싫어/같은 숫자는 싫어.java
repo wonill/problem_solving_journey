6ffbd06f27218b1c621ac84e49a7c4a15fada0ea
wonill <119827402+wonill@users.noreply.github.com>
2023-05-24 01:33:14 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 27.67 ms, Memory: 118 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        int[] answer = {};
+        List<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] == arr[i-1]){
+                continue;
+            }
+            list.add(arr[i]);
+        }
+        answer = new int[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+}
