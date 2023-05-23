8825e666a0e7f147a59079b889ed897f96a2e8cb
wonill <119827402+wonill@users.noreply.github.com>
2023-05-24 01:17:21 +0900

    [level 1] Title: 2016년, Time: 0.02 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.java
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -1,35 +1,12 @@
 class Solution {
     public String solution(int a, int b) {
-        String answer = "";
         String[] dayOfWeek = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
-        if(a == 1){
-            answer = dayOfWeek[(b+4)%7];
-        } else if (a == 2){
-            answer = dayOfWeek[b%7];
-        } else if (a == 3){
-            answer = dayOfWeek[(b+1)%7];
-        }  else if (a == 4){
-            answer = dayOfWeek[(b+4)%7];
-        } else if (a == 5){
-            answer = dayOfWeek[(b-1)%7];
-        } else if (a == 6){
-            answer = dayOfWeek[(b+2)%7];
-        } else if (a == 7){
-            answer = dayOfWeek[(b+4)%7];
-        } else if (a == 8){
-            answer = dayOfWeek[b%7];
-        } else if (a == 9){
-            answer = dayOfWeek[(b+3)%7];
-        } else if (a == 10){
-            answer = dayOfWeek[(b+5)%7];
-        } else if (a == 11){
-            answer = dayOfWeek[(b+1)%7];
-        } else if (a == 12){
-            answer = dayOfWeek[(b+3)%7];
-        } 
-        
-        
-        return answer;
+        int[] last_days = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int days = 0;
+        for(int i = 0; i < a; i++){
+            days += i == a-1 ? b : last_days[i];
+        }
+        return dayOfWeek[(days+4)%7];
         //1 31  1일 금요일
         //2 29  1일 월요일
         //3 31  1일 화요일
