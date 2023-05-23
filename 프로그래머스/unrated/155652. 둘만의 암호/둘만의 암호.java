a91a7701f39669122bbf472aaaa43ffe5a4c055c
wonill <119827402+wonill@users.noreply.github.com>
2023-05-23 20:08:14 +0900

    [unrated] Title: 둘만의 암호, Time: 1.12 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -1,36 +1,21 @@
 import java.util.*;
 class Solution {
     public String solution(String s, String skip, int index) {
-        String answer = "";
-        
-        Map<Character, Boolean> skipMap = new HashMap<>();
-        
-        for(int i = 0; i < skip.length(); i++){
-            skipMap.put(skip.charAt(i), true);
-        }
-        
-        for(int i = 0; i < s.length(); i++){
-            char ch = s.charAt(i);
-     
-            for(int j = 0; j < index; j++){
-                ch = (char)(ch + 1);
-                
-                if(ch > 'z'){
-                    ch = (char)(ch - 26);
-                }
-                
-              
-                if(skipMap.containsKey(ch)){
-                    j--;
-                    continue;
-                }
-                
-                
-            }
-            
-            answer+= ch;
-        }
-        
-        return answer;
+        StringBuilder answer = new StringBuilder();
+		
+		for(char letter : s.toCharArray()) {
+			//char temp = letter;
+			int idx = 0;
+			while(idx < index) {
+				letter = letter == 'z' ? 'a' : (char)(letter + 1);
+				if(!skip.contains(String.valueOf(letter))) {
+					idx += 1;
+				}
+			}
+			answer.append(letter);
+		}
+		
+		
+		return answer.toString();
     }
 }
