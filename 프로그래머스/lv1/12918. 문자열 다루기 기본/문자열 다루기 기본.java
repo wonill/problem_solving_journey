7ce1640361d3bda8ef8df868d6448ef280e3606c
wonill <119827402+wonill@users.noreply.github.com>
2023-05-23 20:53:04 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.09 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,14 +1,5 @@
 class Solution {
     public boolean solution(String s) {
-        boolean answer = true;
-        for(char ch : s.toCharArray()){
-            if(!Character.isDigit(ch)){
-                return false;
-            }
-            if(s.length() != 4 && s.length() != 6){
-                return false;
-            }
-        }
-        return answer;
+        return s.matches("\\d{4}|\\d{6}"); 
     }
 }
