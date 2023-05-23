ef8cb560fff0fc4ec0f60f76428a1c9539b49e8f
wonill <119827402+wonill@users.noreply.github.com>
2023-05-23 17:00:23 +0900

    [unrated] Title: 카드 뭉치, Time: 0.02 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.java
@@ -3,23 +3,17 @@ class Solution {
         String answer = "";
         int cards1_index = 0;
         int cards2_index = 0;
-        
         for(int i = 0; i < goal.length; i++){
             if(cards1_index < cards1.length && goal[i].equals(cards1[cards1_index])){
                 cards1_index++;
-                continue;
             } else if(cards2_index < cards2.length && goal[i].equals(cards2[cards2_index])){
                 cards2_index++;
-                continue;
+            } else {
+                return "No";
             }
             
-            answer="No";
-            break;
         }
-        
-        if(answer.equals("")){
-            answer = "Yes";
-        }
+        answer = "Yes";
         return answer;
     }
 }
