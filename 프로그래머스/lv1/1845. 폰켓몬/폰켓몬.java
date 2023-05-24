e3e4696886717cf16e32e9dfecccfe018cf8f7d7
wonill <119827402+wonill@users.noreply.github.com>
2023-05-24 18:22:36 +0900

    [level 1] Title: 폰켓몬, Time: 4.89 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -1,18 +1,10 @@
 import java.util.*;
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
-        Set<Integer> numsSet = new HashSet<>();
+         Set<Integer> numsSet = new HashSet<>();
         for(int num : nums){
             numsSet.add(num);    
         }
-        Iterator<Integer> iter = numsSet.iterator();
-        while(iter.hasNext()){
-            iter.next();
-            answer++;
-        }
-        
-        
-        return answer > nums.length/2 ? nums.length/2 : answer;
+        return numsSet.size() > nums.length/2 ? nums.length/2 : numsSet.size();
     }
 }
