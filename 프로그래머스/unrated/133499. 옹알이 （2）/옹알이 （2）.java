de48007b900a25a0b5053e4d722cb4a11a88ba37
wonill <119827402+wonill@users.noreply.github.com>
2023-05-24 23:05:01 +0900

    [unrated] Title: 옹알이 (2), Time: 0.74 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,39 +1,19 @@
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-        String[] validWords = {"aya", "ye", "woo", "ma"};
-		for(String word : babbling) {
-			String pre_word = "";
-			while(true) {
-				if(word.length() <= 3) {
-					if(word.equals("aya") || word.equals("ye") || word.equals("woo") || word.equals("ma")) {
-						if(!word.equals(pre_word)) {
-							answer++;							
-						}
-						break;
-					} else {
-						break;
-					}
-				}
-				
-				if(word.substring(0,3).equals("aya") && !pre_word.equals("aya")) {
-					pre_word = "aya";
-					word = word.substring(3);
-				} else if(word.substring(0,2).equals("ye") && !pre_word.equals("ye")) {
-					pre_word = "ye";
-					word = word.substring(2);
-				} else if(word.substring(0, 3).equals("woo") && !pre_word.equals("woo")) {
-					pre_word = "woo";
-					word = word.substring(3);
-				} else if(word.substring(0, 2).equals("ma") && !pre_word.equals("ma")) {
-					pre_word = "ma";
-					word = word.substring(2);
-				} else {
-					break;
-				}
-			}
-			
-		}
+		for(int i = 0; i < babbling.length; i++){
+            if(babbling[i].contains("ayaaya") || babbling[i].contains("yeye") ||
+              babbling[i].contains("woowoo") || babbling[i].contains("mama")){
+                continue;
+            }   
+            babbling[i] = babbling[i].replace("aya"," ");           
+            babbling[i] = babbling[i].replace("ye", " ");    
+            babbling[i] = babbling[i].replace("woo", " ");  
+            babbling[i] = babbling[i].replace("ma", " ");
+            babbling[i] = babbling[i].replace(" ", "");
+            if(babbling[i].length() == 0) answer++;
+        }
+		
         return answer;
     }
 }
