39d87d4cd522f16f5db3433a204e03cc965886f3
wonill <119827402+wonill@users.noreply.github.com>
2023-05-25 04:55:24 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 3.59 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -6,34 +6,35 @@ class Solution {
         for(int i = 0; i < numbers.length; i++) {
         	int x = numbers[i] == 0? 3 : (numbers[i]-1)/3;
         	int y = numbers[i] == 0? 1 : (numbers[i]+2)%3; 
-        	if(numbers[i] % 3 == 1) {
+        	
+        	if(getSuitableFinger(x, y, left, right, numbers[i], hand).equals("left")) {
         		answer += "L";
         		left[0] = x;
         		left[1] = y;
-        	} else if(numbers[i] % 3 == 0 && numbers[i] != 0) {
+        	} else {
         		answer += "R";
         		right[0] = x;
         		right[1] = y;
-        	} else if(Math.abs(left[0]-x) + Math.abs(left[1]-y) < Math.abs(right[0]-x) + Math.abs(right[1]-y)) {
-        		answer += "L";
-        		left[0] = x;
-        		left[1] = y;
-        	} else if(Math.abs(left[0]-x) + Math.abs(left[1]-y) > Math.abs(right[0]-x) + Math.abs(right[1]-y)) {
-        		answer += "R";
-        		right[0] = x;
-        		right[1] = y;
-        	} else if(Math.abs(left[0]-x) + Math.abs(left[1]-y) == Math.abs(right[0]-x) + Math.abs(right[1]-y)) {
-        		if(hand.equals("left")) {
-        			answer += "L";
-            		left[0] = x;
-            		left[1] = y;
-        		} else {
-        			answer += "R";
-            		right[0] = x;
-            		right[1] = y;
-        		}
         	}
+        	
         }
         return answer;
     }
+    
+    public String getSuitableFinger(int x, int y, int[] left, int[] right, int number, String hand) {
+		if(number % 3 == 1) {
+    		return "left";
+    	} else if(number % 3 == 0 && number != 0) {
+    		return "right";
+    		
+    	} else if(Math.abs(left[0]-x) + Math.abs(left[1]-y) < Math.abs(right[0]-x) + Math.abs(right[1]-y)) {
+    		return "left";
+    		
+    	} else if(Math.abs(left[0]-x) + Math.abs(left[1]-y) > Math.abs(right[0]-x) + Math.abs(right[1]-y)) {
+    		return "right";
+    		
+    	} else {
+    		return hand;
+    	}
+	}
 }
