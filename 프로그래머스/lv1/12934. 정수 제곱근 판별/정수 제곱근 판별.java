17dce3a2a11427b60d0837037d7882f686c59390
wonill <119827402+wonill@users.noreply.github.com>
2023-05-24 18:55:01 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,7 @@
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        double sqrt = Math.sqrt(n);
+        return answer = (long)(sqrt == (int)(sqrt) ? (sqrt+1)*(sqrt+1) : -1);
+    }
+}
