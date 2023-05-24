058a442ef4aa133d2482a97520f7eecfd0aa49de
wonill <119827402+wonill@users.noreply.github.com>
2023-05-24 18:39:05 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 14.84 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        for(int i = 1; i < food.length; i++){
+            for(int j = 0; j < food[i]/2; j++){
+                answer += i;
+            }
+        }
+        String reverseAnswer = new StringBuilder(answer).reverse().toString();
+        return answer += 0 + reverseAnswer;
+    }
+}
