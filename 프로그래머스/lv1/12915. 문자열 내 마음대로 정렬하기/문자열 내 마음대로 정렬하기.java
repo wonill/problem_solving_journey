c7408d8d186cf0fc10b4be13d363a2ccf9bf53ab
wonill <119827402+wonill@users.noreply.github.com>
2023-05-26 17:25:28 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.56 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -1,30 +1,15 @@
 import java.util.*;
 class Solution {
     public String[] solution(String[] strings, int n) {
-        String[] answer = new String[strings.length];
-		char[] chs = new char[strings.length];
-		int[] origin_idx = new int[strings.length];
-		Arrays.sort(strings);
-		for(int i = 0; i < strings.length; i++) {
-			chs[i] = strings[i].charAt(n);
-			origin_idx[i] = i;
-		}
-		for(int i = 0; i < chs.length-1; i++) {
-			for(int j = 0; j < chs.length-i-1; j++) {
-				if(chs[j] > chs[j+1]) {
-					char tmp = chs[j];
-					chs[j] = chs[j+1];
-					chs[j+1] = tmp;
-					
-					int tmp2 = origin_idx[j];
-					origin_idx[j] = origin_idx[j+1];
-					origin_idx[j+1] = tmp2;
-				}
+        Arrays.sort(strings, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				if(o1.charAt(n) > o2.charAt(n)) return 1;
+				if(o1.charAt(n) == o2.charAt(n)) return o1.compareTo(o2);
+				if(o1.charAt(n) < o2.charAt(n)) return -1;
+				return 0;
 			}
-		}
-		for(int i = 0; i < strings.length; i++) {
-			answer[i] = strings[origin_idx[i]];
-		}
-        return answer;
+		});
+		return strings;
     }
 }
