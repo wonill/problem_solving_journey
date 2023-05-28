c484a7272170f143dbb26861d69a1f495a843335
wonill <119827402+wonill@users.noreply.github.com>
2023-05-28 15:28:05 +0900

    [unrated] Title: 점 찍기, Time: 0.28 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/unrated/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/unrated/140107. 점 찍기/점 찍기.java
@@ -1,7 +1,6 @@
 class Solution {
     public long solution(int k, int d) {
         long answer = 0;
-        double n = d/(double)k;
         for(double i = 0; i <= d; i+=k) {
         	answer += Math.floor(Math.sqrt((Math.pow(d, 2) - Math.pow(i, 2)))/k) + 1;
         }
