14ee12bc7790adf89e64707fc14a97cf5bbcecb5
wonill <119827402+wonill@users.noreply.github.com>
2023-06-14 04:13:37 +0900

    [unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -3,7 +3,7 @@ function solution(n) {
     const isEven = n % 2 == 0? true : false;
     for(i = 1; i <= n; i++){
         if(isEven){
-            answer += i%2 == 0 ? i*i : 0;
+            answer += i%2 == 0 ? i**2 : 0;
         } else{
             answer += i%2 == 1 ? i : 0;
         }
