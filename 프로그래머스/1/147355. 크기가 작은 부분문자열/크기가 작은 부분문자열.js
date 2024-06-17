6eaa87061507de452d5832ad88bbc22ca6c084ef
wonill <119827402+wonill@users.noreply.github.com>
2024-06-17 23:55:50 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -0,0 +1,7 @@
+function solution(t, p) {
+    var answer = 0;
+    for (let i = 0; i < t.length - p.length + 1; i++){
+        if (Number(t.substr(i, p.length)) <= Number(p)) answer++; 
+    }
+    return answer;
+}
