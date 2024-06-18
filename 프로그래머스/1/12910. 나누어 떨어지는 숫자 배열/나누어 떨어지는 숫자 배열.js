a2bf082fd2815e54440daddbe7851d615efb4c26
wonill <119827402+wonill@users.noreply.github.com>
2024-06-18 18:24:44 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
@@ -1,7 +1,6 @@
 function solution(arr, divisor) {
-    let answer = [];
     //for (a of arr) if (a % divisor === 0) answer.push(a);
-    answer = arr.filter(value => value % divisor === 0)
+    let answer = arr.filter(value => value % divisor === 0)
     // if (answer.length === 0) return [-1];
     return answer.length === 0 ? [-1] : answer.sort((a, b) => a - b);
 }
