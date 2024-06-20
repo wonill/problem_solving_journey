75f38357b12433e10d0e11fab5c74c592b4c72ed
wonill <119827402+wonill@users.noreply.github.com>
2024-06-21 04:02:55 +0900

    [level 1] Title: 최소직사각형, Time: 5.82 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,10 +1,4 @@
 function solution(sizes) {
-    sizes.forEach(v => {
-        if (v[0] < v[1]){
-            let tmp = v[0];
-            v[0] = v[1];
-            v[1] = tmp;
-        }
-    });
+    sizes.forEach(v => v.sort((a, b) => a - b));
     return sizes.reduce((a, b) => Math.max(a, b[0]), 0) * sizes.reduce((a, b) => Math.max(a, b[1]), 0);
 }
