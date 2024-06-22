4f7da908f8bc230f1a3cc5ed6399053f6908a098
wonill <119827402+wonill@users.noreply.github.com>
2024-06-22 15:42:57 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,11 @@
+function solution(n){
+    var ans = 0;
+    while (n > 0){
+        if (n % 2 === 0) n /= 2;
+        else {
+            n -= 1;
+            ans++;
+        }
+    }
+    return ans;
+}
