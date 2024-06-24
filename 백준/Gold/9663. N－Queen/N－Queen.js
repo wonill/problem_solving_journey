be095daa605d6706290fb0fbe5655f09fe6e81c8
wonill <119827402+wonill@users.noreply.github.com>
2024-06-25 02:44:43 +0900

    [Gold IV] Title: N-Queen, Time: 20800 ms, Memory: 14972 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.js
@@ -0,0 +1,26 @@
+let fs = require('fs');
+let n = Number(fs.readFileSync('/dev/stdin').toString());
+
+let queens = [];
+let count = 0;
+
+function possible(a, b){
+  for (let [x, y] of queens){
+    if(x === a || y === b) return false;
+    if(Math.abs(x - a) === Math.abs(y - b)) return false;
+  }
+  return true;
+}
+
+function dfs(row){
+  if (row === n) count++;
+  for (let i = 0; i < n; i++){
+    if(!possible(row, i)) continue;
+    queens.push([row, i]);
+    dfs(row + 1);
+    queens.pop();
+  }
+}
+
+dfs(0);
+console.log(count);
