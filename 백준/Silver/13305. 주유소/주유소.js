f5ebcd612e4acd75b648e73891e37896d7a46bad
wonill <119827402+wonill@users.noreply.github.com>
2024-06-29 03:18:40 +0900

    [Silver III] Title: 주유소, Time: 260 ms, Memory: 37048 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -4,11 +4,11 @@ let n = Number(input[0]);
 let distances = input[1].split(' ').map(Number);
 let prices = input[2].split(' ').map(Number);
 
-let answer = 0;
-let minPrice = 0;
+let answer = BigInt(0);
+let minPrice = prices[0];
 for (let i = 0; i < n - 1; i++){
-  if (minPrice === 0 || prices[i] < minPrice) minPrice = prices[i];
-  answer += minPrice * distances[i];
+  minPrice = Math.min(minPrice, prices[i]);
+  answer += BigInt(minPrice) * BigInt(distances[i]);
 }
 
-console.log(answer);
+console.log(answer.toString());
