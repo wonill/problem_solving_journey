5a87196d28c2128c11098b86cfc94aabfa190b90
wonill <119827402+wonill@users.noreply.github.com>
2024-06-30 05:47:20 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 19.66 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
@@ -1,6 +1,6 @@
 function solution(arr, queries) {
-    return arr.map((v, i) =>{
-       for (let query of queries) if (i >= query[0] && i <= query[1]) v++;
-       return v;
+    queries.forEach(([s, e]) => {
+       while(s <= e) arr[s++]++; 
     });
+    return arr;
 }
