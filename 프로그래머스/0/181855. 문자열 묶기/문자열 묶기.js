09c87f111dee1a6d63c5d3dac6db7d2cd0fa0fb6
wonill <119827402+wonill@users.noreply.github.com>
2024-07-03 02:29:50 +0900

    [level 0] Title: 문자열 묶기, Time: 7.89 ms, Memory: 51.3 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
@@ -1,7 +1,7 @@
 function solution(strArr) {
     let map = new Map();
     for (let str of strArr){
-        map.get(str.length) ? map.set(str.length, map.get(str.length) + 1) : map.set(str.length, 1); 
+        map.set(str.length, (map.get(str.length) || 0) + 1);
     }
     return Math.max(...[...map].map(v => v[1]));
 }
