8e53b11d73fa35bf09be48734c276bc4dcecafa9
wonill <119827402+wonill@users.noreply.github.com>
2024-07-03 14:52:41 +0900

    [level 0] Title: 배열 만들기 6, Time: 4.28 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
@@ -0,0 +1,8 @@
+function solution(arr) {
+    var answer = [];
+    for (let i = 0; i < arr.length; i++){
+        if (answer.length && answer[answer.length - 1] === arr[i]) answer.pop();
+        else answer.push(arr[i]);
+    }
+    return answer.length ? answer : [-1];
+}
