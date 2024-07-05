3455a0e4075112709cf95a54d7718c32cca6407a
wonill <119827402+wonill@users.noreply.github.com>
2024-07-06 01:30:30 +0900

    [level 2] Title: 모음 사전, Time: 4.44 ms, Memory: 34.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,26 +1,12 @@
-const wordMap = {
-  '0' : '',
-  '1' : 'A',
-  '2' : 'E',
-  '3' : 'I',
-  '4' : 'O',
-  '5' : 'U'
-}
+const alphabets = ['', 'A', 'E', 'I', 'O', 'U'];
+
+const f = (alphabet) => alphabets.map(v => v + alphabet);
 
 function solution(word) {
-    let words = new Set();
-    for (let i = 0; i < 6; i++){
-      for (let j = 0; j < 6; j++){
-        for (let k = 0; k < 6; k++){
-          for (let l = 0; l < 6; l++){
-            for (let m = 0; m < 6; m++){
-            words.add(wordMap[i] + wordMap[j] + 
-                    wordMap[k] + wordMap[l] + wordMap[m]);
-            }
-          }
-        }
-      }
-    }
-    let dictionary = [...words].sort();
-    return dictionary.indexOf(word);
+    return Array.from(new Set(alphabets.map(f).flat()
+                    .map(f).flat()
+                    .map(f).flat()
+                    .map(f).flat())).sort().indexOf(word);   
+    
+    
 }
