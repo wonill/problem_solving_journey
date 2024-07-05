7c0ba4e4780e11c46fd71e262b82c5d73ca394fc
wonill <119827402+wonill@users.noreply.github.com>
2024-07-05 22:52:32 +0900

    [level 2] Title: 모음 사전, Time: 4.96 ms, Memory: 37.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -0,0 +1,26 @@
+const wordMap = {
+  '0' : '',
+  '1' : 'A',
+  '2' : 'E',
+  '3' : 'I',
+  '4' : 'O',
+  '5' : 'U'
+}
+
+function solution(word) {
+    let words = new Set();
+    for (let i = 0; i < 6; i++){
+      for (let j = 0; j < 6; j++){
+        for (let k = 0; k < 6; k++){
+          for (let l = 0; l < 6; l++){
+            for (let m = 0; m < 6; m++){
+            words.add(wordMap[i] + wordMap[j] + 
+                    wordMap[k] + wordMap[l] + wordMap[m]);
+            }
+          }
+        }
+      }
+    }
+    let dictionary = [...words].sort();
+    for (let i = 0; i < dictionary.length; i++) if (word === dictionary[i]) return i;
+}
