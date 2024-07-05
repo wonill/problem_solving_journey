df8b965f77ee0cc72a52812d83a07c64323f48d6
wonill <119827402+wonill@users.noreply.github.com>
2024-07-05 21:41:16 +0900

    [level 2] Title: 전화번호 목록, Time: 115.50 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,20 +1,5 @@
 function solution(phone_book) {
-    let phoneBook = Array.from({length: 21}, () => []);
-    for (let i = 0; i < phone_book.length; i++) {
-        phoneBook[phone_book[i].length].push(phone_book[i]);
-    }
-    for (let i = 20; i >= 1; i--){
-        if (phoneBook[i].length){
-            let set = new Set(phoneBook[i]);
-            for (let j = i + 1; j <= 20; j++){
-                if (phoneBook[j].length){
-                    let prefixes = new Set(phoneBook[j].map(v => v.slice(0, i)));
-                    let tmp = new Set(set);
-                    for (let prefix of prefixes) tmp.add(prefix);
-                    if (set.size + prefixes.size != tmp.size) return false;
-                }
-            }
-        }
-    }
+    phone_book.sort();
+    for (let i = 0; i < phone_book.length; i++) if (i > 0 && phone_book[i].startsWith(phone_book[i - 1])) return false;
     return true;
 }
