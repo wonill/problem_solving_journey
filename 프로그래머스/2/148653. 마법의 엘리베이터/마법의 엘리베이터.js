6c9d90834805a338add402b5beb00d620cb8bbe6
wonill <119827402+wonill@users.noreply.github.com>
2024-07-07 11:52:12 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
@@ -0,0 +1,31 @@
+function createFive(n){
+    if (n === 1) return 5;
+    let num = '';
+    for (let i = 0; i < n; i++){
+        if (i === n - 1){
+            num += '4';
+            break;
+        }
+        num += '5';
+    }
+    return Number(num);
+}
+
+function solution(storey) {
+    var answer = 0;
+    let power = storey.toString().length;
+    while (power >= 0){
+        let move = 0;
+        while (10 ** power * move <= storey) move++;
+        let a = 10 ** power * move;
+        let b = 10 ** power * (move - 1);
+        if(storey - b <= createFive(power)){
+            move -= 1;
+            storey -= b;
+        }
+        else storey = a - storey;
+        answer += move;
+        power--;
+    }
+    return answer;
+}
