71a809b300eb686d8f87e221c8177f12a2c4a16e
wonill <119827402+wonill@users.noreply.github.com>
2024-07-07 14:12:22 +0900

    [level 2] Title: 호텔 대실, Time: 4.66 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
@@ -5,9 +5,10 @@ function makeMinTimeStamp(time){
 
 function solution(book_time) {
     const timeArr = Array(makeMinTimeStamp('23:59' + 9)).fill(0);
-    book_time.forEach((v, i) =>{
-        let start = makeMinTimeStamp(v[0]);
-        let end = makeMinTimeStamp(v[1]) + 9;
+    book_time.forEach((time, i) =>{
+        const [s, e] = time;
+        let start = makeMinTimeStamp(s);
+        const end = makeMinTimeStamp(e) + 9;
         
         for (start; start <= end; start++){
             timeArr[start]++;
