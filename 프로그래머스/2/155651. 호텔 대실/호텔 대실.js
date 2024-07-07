341759878a7e70cd5eb76a55cbe8cf898275b467
wonill <119827402+wonill@users.noreply.github.com>
2024-07-07 14:10:33 +0900

    [level 2] Title: 호텔 대실, Time: 4.42 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
@@ -1,36 +1,17 @@
-function addCleaningTime(time){
-    let end_minutes = Number(time[1].split(':')[1]);
-    if (end_minutes + 9 >= 60){
-        time[1] = (Number(time[1].split(':')[0]) + 1) + ':'  
-            + (Number(time[1].split(':')[1]) - 51).toString().padStart(2, '0');
-    }
-    else{
-        time[1] = time[1].split(':')[0] + ':'  + (Number(time[1].split(':')[1]) + 9);
-    }
-    return time;
+function makeMinTimeStamp(time){
+    let [hour, minute] = time.split(':').map(Number);
+    return hour * 60 + minute;
 }
 
-function isTimeOverlapped(hour, minute, time){
-    let start = time[0].split(':').map(Number);
-    let end = time[1].split(':').map(Number);
-    if (hour < start[0]) return false;
-    if (hour === start[0] && minute < start[1]) return false;
-    if (hour > end[0]) return false;
-    if (hour === end[0] && minute > end[1]) return false;
-    return true;
-}  
-
 function solution(book_time) {
-    book_time = book_time.map(v => addCleaningTime(v));
-    let overlapped_counts = [];
-    for (let hour = 0; hour < 24; hour++){
-        for (let minute = 0; minute < 60; minute++){
-            let count = 0;
-            for (let time of book_time){
-                if(isTimeOverlapped(hour, minute, time)) count++;
-            }
-            overlapped_counts.push(count);
+    const timeArr = Array(makeMinTimeStamp('23:59' + 9)).fill(0);
+    book_time.forEach((v, i) =>{
+        let start = makeMinTimeStamp(v[0]);
+        let end = makeMinTimeStamp(v[1]) + 9;
+        
+        for (start; start <= end; start++){
+            timeArr[start]++;
         }
-    }
-    return Math.max(...overlapped_counts);
+    });
+    return Math.max(...timeArr);
 }
