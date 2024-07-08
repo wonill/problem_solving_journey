ccc24f07e56931813ebd6df63d693914102b76aa
wonill <119827402+wonill@users.noreply.github.com>
2024-07-08 21:18:29 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,30 +1,24 @@
-const parentheses = {
-    ']' : '[',
-    '}' : '{',
-    ')' : '('
-}
-
-function isCorrectParenthesesStr(str, start, end){
-    let parenthesesStack = [];
-    for (start; start <= end; start++){
-        if (str[start] == '[' || str[start] == '{' || str[start] == '(') parenthesesStack.push(str[start]);
-        else if (parenthesesStack[parenthesesStack.length - 1] == parentheses[str[start]]) parenthesesStack.pop();
-        else return false;
-    }
-    if (parenthesesStack.length) return false;
-    return true;
-}
 
 function solution(s) {
     let answer = 0;
-    let start = 0;
-    let end = s.length - 1;
-    const len = s.length;
-    for (let i = 0; i < len; i++){
-        s += s[start];
-        start++;
-        end++;
-        if(isCorrectParenthesesStr(s, start, end)) answer++;
+    if (s.length % 2 === 1) return 0;
+    const mapping = {']' : '[', '}' : '{', ')' : '('};
+    
+    for (let i = 0; i < s.length; i++){
+        const stack = [];
+        const rotate = s.slice(i) + s.slice(0, i);
+        let flag = true;
+        for (j = 0; j < s.length; j++){
+            if (rotate[j] === '[' || rotate[j] === '{' || rotate[j] === '(') stack.push(rotate[j]);
+            else {
+                let last = stack.pop();
+                if (last != mapping[rotate[j]]){
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        if (flag) answer++;
     }
     return answer;
 }
