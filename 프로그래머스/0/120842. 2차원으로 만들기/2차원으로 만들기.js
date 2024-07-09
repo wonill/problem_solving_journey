b63b59bbd62c18e9a84a15870a8470a31c7cd6bc
wonill <119827402+wonill@users.noreply.github.com>
2024-07-10 02:21:25 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,4 +1,3 @@
 function solution(num_list, n) {
-    return Array.from({length : num_list.length / n}, (_, i) => Array.from({length : n}, (_, j) => num_list[n * i + j]));
-                      
+    return Array(num_list.length / n).fill([]).map(() => num_list.splice(0, n));
 }
