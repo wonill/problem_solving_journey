6c80466ddc3350b47e69e3b03666f2a8feb03e8f
wonill <119827402+wonill@users.noreply.github.com>
2024-07-09 21:59:56 +0900

    [level 0] Title: 두 수의 합, Time: 39.01 ms, Memory: 34.3 MB -BaekjoonHub

--- 프로그래머스/0/181846. 두 수의 합/두 수의 합.js
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.js
@@ -1,12 +1,3 @@
 function solution(a, b) {
-    var answer = '';
-    let flag = false;
-    a = a.padStart(Math.max(a.length, b.length), '0');
-    b = b.padStart(Math.max(a.length, b.length), '0');
-    for (let i = a.length - 1; i >= 0; i--){
-        let sum = Number(a[i]) + Number(b[i]);
-        answer = (sum + +flag) % 10 + answer;
-        flag = sum >= 10 || (sum % 10 + +flag) >= 10 ? true : false;
-    }
-    return flag ? 1 + answer : answer;
+    return BigInt(a) + BigInt(b) + '';
 }
