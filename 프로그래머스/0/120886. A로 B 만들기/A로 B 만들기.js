0be68248ae029e5d21c4336c7ab94de647c055c1
wonill <119827402+wonill@users.noreply.github.com>
2024-07-10 00:12:58 +0900

    [level 0] Title: A로 B 만들기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,1 +1,1 @@
-const solution = (before, after) => +(before.split('').reduce((a, c) => a=a.replace(c, '') ,after).length == 0);
+const solution = (before, after) => +(before.split('').sort().join('') === after.split('').sort().join(''));
