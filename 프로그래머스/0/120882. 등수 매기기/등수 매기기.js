9ca2c301b7cb8a7ef065c64168c96577660b5596
wonill <119827402+wonill@users.noreply.github.com>
2024-07-12 13:05:14 +0900

    [level 0] Title: 등수 매기기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
@@ -1,14 +1,5 @@
 function solution(score) {
-    let averages = score.map(([e, m]) => (e + m) / 2);
-    let grades = [...averages];
-    grades.sort((a, b) => b - a);
-    let map = new Map();
-    let tmp;
-    for (let i = 0; i < grades.length; i++) {
-        if (i > 0 && grades[i - 1] == grades[i]) tmp++;
-        else map.set(grades[i], i + 1);
-        tmp = i + 1;
-    }
-    for(let i = 0; i < averages.length; i++) averages[i] = map.get(averages[i]);
-    return averages;
+    return score.map(a => {
+       return score.filter(b => (b[0] + b[1]) / 2 > (a[0] + a[1]) / 2).length + 1; 
+    });
 }
