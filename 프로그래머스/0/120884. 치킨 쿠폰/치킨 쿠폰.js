20caf6d825b5e36dcb4f1bc48f3310d747c6de3b
wonill <119827402+wonill@users.noreply.github.com>
2024-07-14 11:53:32 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -0,0 +1,8 @@
+function solution(chicken) {
+    let answer = 0;
+    while (~~(chicken / 10) > 0){
+        answer += ~~(chicken /10);
+        chicken = ~~(chicken / 10) + chicken % 10;
+    }
+    return answer;
+}
