035fc26c3111074688b1d8e5603cfbf3d2cf74ec
wonill <119827402+wonill@users.noreply.github.com>
2024-07-14 13:23:40 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,7 +1,6 @@
 
 
 function solution(numbers) {
-    let numArr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    for (let i = 0; i < 10; i++) numbers = numbers.replaceAll(numArr[i], i);
-    return Number(numbers);
+    return Number(['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'].reduce((a, c, i) =>
+                                        a.replaceAll(c, i) , numbers));
 }
