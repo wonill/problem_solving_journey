5b4aeaf9d9d7c137e3f7481cf897ac457d8a5e27
wonill <119827402+wonill@users.noreply.github.com>
2024-07-14 13:29:38 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.24 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,4 +1,3 @@
 function solution(my_string) {
-    return [...my_string].map(v => v.charCodeAt() <= 90 ? String.fromCharCode(v.charCodeAt() + 32) :
-                             String.fromCharCode(v.charCodeAt() - 32)).join('');
+    return [...my_string].map(v => v.toLowerCase() === v ? v.toUpperCase() : v.toLowerCase()).join('');
 }
