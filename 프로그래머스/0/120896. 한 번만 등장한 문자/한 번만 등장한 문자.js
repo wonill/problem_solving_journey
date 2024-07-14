bf8fbe80d4247f01c485b18712558c89c1e14c9d
wonill <119827402+wonill@users.noreply.github.com>
2024-07-14 13:00:11 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.18 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,6 +1,5 @@
 function solution(s) {
-    let map = new Map();
-    for (let c of s) map.set(c, (map.get(c) || 0) + 1);
-    for (let [key, value] of map) if (value != 1) map.delete(key);
-    return [...map.keys()].sort().join('');
+    let answer = [];
+    for (let c of s) if (s.indexOf(c) === s.lastIndexOf(c)) answer.push(c);
+    return answer.sort().join('');
 }
