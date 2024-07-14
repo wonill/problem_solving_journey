c54ab01117c1bed1061dbb212f324a6ee9cfad43
wonill <119827402+wonill@users.noreply.github.com>
2024-07-14 12:33:29 +0900

    [level 0] Title: 문자열 밀기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,4 +1,3 @@
 function solution(A, B) {
-    for (let i = 0; i < A.length; i++) if (A.slice(A.length - i) + A.slice(0, A.length - i) === B) return i;
-    return -1;
+    return (B+B).indexOf(A);
 }
