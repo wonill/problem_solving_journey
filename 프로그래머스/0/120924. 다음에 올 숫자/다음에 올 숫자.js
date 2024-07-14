3f48d67702695e556925bb9362ccc9010d6c1c54
wonill <119827402+wonill@users.noreply.github.com>
2024-07-14 12:45:30 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -0,0 +1,4 @@
+function solution(common) {
+    if (common[1] - common[0] === common[2] - common[1]) return common[common.length - 1] + common[1] - common[0];
+    if (common[1] / common[0] === common[2] / common[1]) return common[common.length - 1] * common[1] / common[0];
+}
