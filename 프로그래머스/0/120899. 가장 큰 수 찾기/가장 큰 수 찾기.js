058fcff93bc60470c98444e5670e6cf58b6c29cd
wonill <119827402+wonill@users.noreply.github.com>
2024-07-14 13:13:01 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,9 +1,4 @@
 function solution(array) {
-    let max = 0;
-    let max_idx;
-    for (let i = 0; i < array.length; i++) if (array[i] > max) {
-        max = array[i];
-        max_idx = i;
-    }
-    return [max, max_idx];
+    const max = Math.max(...array);
+    return [max, array.indexOf(max)];
 }
