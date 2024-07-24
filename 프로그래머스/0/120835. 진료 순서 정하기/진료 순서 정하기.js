1f406b630f61c5612fe1bca79d539b7a3cd33df1
wonill <119827402+wonill@users.noreply.github.com>
2024-07-24 19:11:57 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,14 +1,9 @@
 function solution(emergency) {
-    let set = new Set();
+    let result = [];
     let arr = [...emergency];
     arr.sort((a, b) => b - a);
     for (let i = 0; i < arr.length; i++){
-        for (let j = 0; j < emergency.length; j++){
-            if (!set.has(j) && arr[i] === emergency[j]) {
-                emergency[j] = i + 1;
-                set.add(j);
-            }
-        }
+        result.push(arr.indexOf(emergency[i]) + 1);
     }
-    return emergency;
+    return result;
 }
