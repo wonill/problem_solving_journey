d89238306653ab30f155e2192c32af55137ff513
wonill <119827402+wonill@users.noreply.github.com>
2024-07-24 19:27:17 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -1,16 +1,3 @@
 function solution(hp) {
-    var answer = 0;
-    while (hp >= 5) {
-        hp -= 5;
-        answer++;
-    }
-    while (hp >= 3){
-        hp -=3;
-        answer++;
-    }
-    while (hp >= 1){
-        hp--;
-        answer++;
-    }
-    return answer;
+    return ~~(hp / 5) + ~~(hp % 5 / 3)  + ~~(hp % 5 % 3);
 }
