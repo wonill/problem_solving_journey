1fcc51e248f46541c5942ca59b1c37df8e4906f1
wonill <119827402+wonill@users.noreply.github.com>
2024-07-31 06:58:58 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.35 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.js
@@ -0,0 +1,24 @@
+function solution(n) {
+    const dx = [0, -1, 0, 1];
+    const dy = [1, 0, -1, 0];
+    
+    let answer = Array.from({length : n}, v => Array(n).fill(0));
+    let value = 1;
+    
+    let x = 0;
+    let y = 0;
+    answer[x][y] = value;
+    while (true) {
+        for (let i = 0; i < 4; i++){
+            while (x + dx[i] >= 0 && y + dy[i] >= 0 
+                   && x + dx[i] < n && y + dy[i] < n 
+                   && answer[x + dx[i]][y + dy[i]] === 0){
+                x += dx[i];
+                y += dy[i];
+                answer [x][y] = ++value;
+            }
+        }
+        if (value === n ** 2) break;
+    }
+    return answer;
+}
