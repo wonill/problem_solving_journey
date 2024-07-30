fdee59a4f8886c4892e28ada1e0c1a5febed04ad
wonill <119827402+wonill@users.noreply.github.com>
2024-07-31 05:12:07 +0900

    [level 0] Title: 약수 구하기, Time: 1.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
@@ -1,7 +1,3 @@
 function solution(n) {
-    let result = [];
-    for (let i = 1; i <= n; i++){
-        if (n % i === 0) result.push(i);
-    }
-    return result;
+    return Array.from({length : n}, (_, i) => i + 1).filter(v => n % v === 0);
 }
