d1a899d1e141b0f7f5e65015c4f402f8da9764f1
wonill <119827402+wonill@users.noreply.github.com>
2024-08-03 01:24:04 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -1,14 +1,6 @@
 function solution(a, b) {
-    const max = Math.min(a, b);
-    for (let i = 2; i <= max; i++){
-        while (a % i === 0 && b % i === 0){
-            a /= i;
-            b /= i;
-        }
-    }
-    while (b % 2 === 0 || b % 5 === 0){
-        if (b % 2 === 0) b /= 2;
-        if (b % 5 === 0) b /= 5;
-    }
+    while (b % 2 === 0) b /= 2;
+    while (b % 5 === 0) b /= 5;
+    if (a % b === 0) b = 1;
     return b === 1 ? 1: 2;
 }
