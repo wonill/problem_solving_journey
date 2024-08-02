b6f1a65c24f977d79f7b49c1b00dd32cca60c230
wonill <119827402+wonill@users.noreply.github.com>
2024-08-03 00:41:22 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,7 +1,4 @@
-const extractNumbers = (str) => {
-    return str.match(/\d+/g) || [];
-}
-
 function solution(my_string) {
-    return my_string.match(/\d+/g)?.reduce((a, c) => a + Number(c), 0) || 0;
+    return my_string.split(/\D+/).reduce((a, c) => a + Number(c), 0);
+    // return my_string.match(/\d+/g)?.reduce((a, c) => a + Number(c), 0) || 0;
 }
