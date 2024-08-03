d9ec390208500aa17a0a01a9d3f3e0e6a189041f
wonill <119827402+wonill@users.noreply.github.com>
2024-08-04 05:47:40 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,9 +1,4 @@
 function solution(numbers) {
-    const negative_nums = numbers.filter(v => v < 0)?.sort((a, b) => a - b);
-    const positive_nums = numbers.filter(v => v >= 0)?.sort((a, b) => b - a);
-    if (negative_nums.length < 2) 
-        return positive_nums.length < 2 ? negative_nums[0] * positive_nums[0] : positive_nums[0] * positive_nums[1];
-    if (positive_nums.length < 2)
-        return negative_nums[0] * negative_nums[1];
-    return Math.max(positive_nums[0] * positive_nums[1], negative_nums[0] * negative_nums[1]);
+    numbers.sort((a, b) => a - b);
+    return Math.max(numbers[0] * numbers[1], numbers[numbers.length - 1] * numbers[numbers.length - 2]);
 }
