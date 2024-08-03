e718d96bf3e92ddd7e175502a90fb0490dc5d434
wonill <119827402+wonill@users.noreply.github.com>
2024-08-04 02:49:56 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -1,11 +1,7 @@
 function solution(lines) {
     const field = Array(200).fill(0);
-    for (let line of lines){
-        let start = line[0] + 100;
-        const end = line[1] + 100;
-        for (start; start < end; start++){
-            field[start]++;
-        }
-    }
-    return field.reduce((a, c) => c > 1 ? a += 1 : a, 0);
+    lines.forEach(([a, b]) => {
+        for(;a < b; a++) field[a+100]++;
+    });
+    return field.reduce((a, c) => c > 1 ? ++a : a, 0);
 }
