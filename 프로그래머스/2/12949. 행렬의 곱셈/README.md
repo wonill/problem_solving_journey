c277cc96e55aadf398b19563edb39ea3c1ea9675
wonill <119827402+wonill@users.noreply.github.com>
2024-08-10 00:37:54 +0900

    [level 2] Title: 행렬의 곱셈, Time: 11.75 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/README.md
+++ 프로그래머스/2/12949. 행렬의 곱셈/README.md
@@ -1,10 +1,10 @@
 # [level 2] 행렬의 곱셈 - 12949 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949) 
 
 ### 성능 요약
 
-메모리: 38.4 MB, 시간: 6.27 ms
+메모리: 38.7 MB, 시간: 11.75 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 09일 01:15:21
+2024년 08월 10일 00:37:49
 
 ### 문제 설명
 
--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,22 +1,5 @@
 function solution(arr1, arr2) {
-    var answer = [];
-    for (let i = 0; i < arr1.length; i++){
-        answer.push([]);
-        for (let j = 0; j < arr2[0].length; j++){
-            let sum = 0;
-            for (let k = 0; k < arr2.length; k++){
-                sum += arr1[i][k] * arr2[k][j];
-            }
-            answer[i].push(sum);
-        }
-    }
-    return answer;
+    return arr1.map((row) => arr2[0].map((v, i) => row.reduce((a, b, c) => a + b * arr2[c][i],0)));
 }
 
-// 2 3 2   5 4 3
-// 4 2 4   2 4 1
-// 3 1 4   3 1 1
 
-// 1 4  3 3
-// 3 2  3 3
-// 4 1
