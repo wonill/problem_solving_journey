a168709ba09c3f48a0cf057cb8df2aeabb4eeac5
wonill <119827402+wonill@users.noreply.github.com>
2024-08-14 21:30:43 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 61.12 ms, Memory: 66.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,8 +1,6 @@
 function solution(participant, completion) {
-    participant.sort();
-    completion.sort();
-    for (let i = 0; i < completion.length; i++){
-        if (participant[i] != completion[i]) return participant[i];
-    }
-    return participant.pop();;
+    let map = new Map();
+    for (let p of participant) map.set(p, (map.get(p) || 0) + 1);
+    for (let c of completion) map.set(c, map.get(c) - 1);
+    for (let [key, value] of map) if (value === 1) return key;
 }
