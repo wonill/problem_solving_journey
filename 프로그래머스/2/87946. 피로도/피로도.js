195ec979749a9aa8f25ddae4b5b6e7615d9a73ef
wonil <119827402+wonill@users.noreply.github.com>
2024-08-28 18:18:20 +0900

    [level 2] Title: 피로도, Time: 12.76 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,32 +1,24 @@
-const cases = [];
-const explorableDungeons = [];
-
-const recursion = (caseSet, start, end) => {
-    if (start === end) {
-        cases.push(caseSet);
-        return ;
-    };
-    for (let i = 0; i < end; i++){
-        if (!caseSet.has(i)) {
-            recursion(new Set([...caseSet, i]), start + 1, end);
-        }
-    }
-}
-
 function solution(k, dungeons) {
-    let caseSet = new Set();
-    recursion(caseSet, 0, dungeons.length);
-    cases.forEach(caseSet => {
-        let explorable = 0;
-        let remainFatigue = k;
-        for (const dungeonIdx of caseSet){
-            const minRequiredFatigue = dungeons[dungeonIdx][0];
-            const fatigueUsage = dungeons[dungeonIdx][1];
-            if (remainFatigue < minRequiredFatigue || remainFatigue - fatigueUsage < 0) break;
-            remainFatigue -= fatigueUsage;
-            explorable++;
-        }
-        explorableDungeons.push(explorable);
-    });
-    return Math.max(...explorableDungeons);
+  let max = 0;
+  let indexs = [];
+  let x = 0;
+
+  const dfs = (x) => {
+    if (x === dungeons.length) {
+      max = dungeons.length;
+      return;
+    }
+    for (let i = 0; i < dungeons.length; i++) {
+      if (!indexs.includes(i) && k >= dungeons[i][0]) {
+        indexs.push(i);
+        k -= dungeons[i][1];
+        dfs(x + 1);
+        indexs.pop();
+        k += dungeons[i][1];
+      }
+      max = indexs.length > max ? indexs.length : max;
+    }
+  };
+  dfs(x);
+  return max;
 }
