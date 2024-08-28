4126ed89414c63bdc9bf6931bff218cdf19a7450
wonil <119827402+wonill@users.noreply.github.com>
2024-08-28 18:38:12 +0900

    [level 2] Title: 피로도, Time: 15.70 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,24 +1,19 @@
 function solution(k, dungeons) {
   let max = 0;
-  let indexs = [];
-  let x = 0;
+  let visited = new Set();
 
-  const dfs = (x) => {
-    if (x === dungeons.length) {
-      max = dungeons.length;
-      return;
-    }
+  const dfs = (x, k) => {
+    max = Math.max(visited.size, max);
+
     for (let i = 0; i < dungeons.length; i++) {
-      if (!indexs.includes(i) && k >= dungeons[i][0]) {
-        indexs.push(i);
-        k -= dungeons[i][1];
-        dfs(x + 1);
-        indexs.pop();
-        k += dungeons[i][1];
+      if (!visited.has(i) && k >= dungeons[i][0]) {
+        visited.add(i);
+        dfs(x + 1, k - dungeons[i][1]);
+        visited.delete(i);
       }
-      max = indexs.length > max ? indexs.length : max;
+      max = visited.size > max ? visited.size : max;
     }
   };
-  dfs(x);
+  dfs(0, k);
   return max;
 }
