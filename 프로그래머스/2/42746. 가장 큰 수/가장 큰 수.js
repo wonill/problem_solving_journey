bef20fa9d097da8c0d33952395f4ddbc1fd75628
wonil <119827402+wonill@users.noreply.github.com>
2024-08-29 21:34:13 +0900

    [level 2] Title: 가장 큰 수, Time: 160.34 ms, Memory: 44.9 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,1 +1,7 @@
-const solution = (numbers) => numbers.sort((a, b) => ('' + b + a) - ('' + a + b)).join('').replace(/^0+(?=0)/, '');
+function solution(numbers) {
+		// 내림차순으로 정렬해서 차례대로 숫자를 합치면 끝.
+    var answer = numbers.map((a) => String(a)).sort((a,b) => (b + a) - (a + b)).join('');
+    // 0이 여러개인 예외 케이스
+    if(answer[0] == '0') answer = '0';
+    return answer;
+}
