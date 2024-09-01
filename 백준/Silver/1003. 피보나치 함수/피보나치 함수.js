f9a6b0a35271eeeb304789dfccb5664f1b7cab13
wonil <119827402+wonill@users.noreply.github.com>
2024-09-01 18:18:54 +0900

    [Silver III] Title: 피보나치 함수, Time: 96 ms, Memory: 9928 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.js
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.js
@@ -1,18 +1,15 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const len = input.shift();
-
-for (let i = 0; i < len; i++) {
-    const n = input[i];
-
-    const fibonacci = [[1, 0], [0, 1]];
-    
-    for (let j = 2; j <= n; j++) {
-        fibonacci[j] = [
-            fibonacci[j-1][0] + fibonacci[j-2][0], 
-            fibonacci[j-1][1] + fibonacci[j-2][1]
-        ];
-    }
-
-    console.log(fibonacci[n].join(" "));
+const t = Number(input[0]);
+for (let i = 1; i <= t; i++) {
+  const n = Number(input[i]);
+  let dp = new Array(n);
+  dp[0] = 0;
+  dp[1] = 1;
+  for (let j = 2; j <= n; j++) {
+    dp[j] = dp[j - 1] + dp[j - 2];
+  }
+  if (n === 0) console.log(1, 0);
+  else console.log(dp[n - 1], dp[n]);
 }
