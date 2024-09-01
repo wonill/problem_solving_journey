211ee4baba99073c26004ec17556b23df2e400b3
wonil <119827402+wonill@users.noreply.github.com>
2024-09-02 06:52:45 +0900

    [Silver II] Title: 병사 배치하기, Time: 164 ms, Memory: 9736 KB -BaekjoonHub

--- 백준/Silver/18353. 병사 배치하기/병사 배치하기.js
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.js
@@ -1,22 +1,13 @@
-let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 let n = Number(input[0]);
-let soldiers = input[1].split(' ').map(Number);
+let soldiers = input[1].split(" ").map(Number);
 
-function lowerBound(arr, target){
-  let start = 0;
-  let end = arr.length;
-  while (start < end){
-    let mid = parseInt((start + end) / 2);
-    if (arr[mid] <= target) end = mid;
-    else start = mid + 1;
+let dp = new Array(n).fill(1);
+
+for (let i = 1; i < n; i++) {
+  for (let j = 0; j < i; j++) {
+    if (soldiers[i] < soldiers[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
   }
-  return start;
 }
-
-let result = [];
-for (let soldier of soldiers){
-  if (!result.length || result[result.length - 1] > soldier) result.push(soldier);
-  else result[lowerBound(result, soldier)] = soldier;
-}
-
-console.log(soldiers.length - result.length);
+console.log(n - Math.max(...dp));
