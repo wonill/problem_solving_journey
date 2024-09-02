b620a3acae255e3f34ca9f1e1b137e1362154732
wonil <119827402+wonill@users.noreply.github.com>
2024-09-02 22:24:21 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 100 ms, Memory: 9420 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.js
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.js
@@ -3,23 +3,28 @@ let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 let n = Number(input[0]);
 let arr = input[1].split(" ").map(Number);
 
-function lowerBound(arr, target) {
-  let start = 0;
-  let end = arr.length;
-  while (start < end) {
-    let mid = parseInt((start + end) / 2);
-    if (arr[mid] >= target) end = mid;
-    else start = mid + 1;
-  }
-  return end;
-}
-
 function solution(nums) {
-  let answer = [];
-  for (let i = 0; i < nums.length; i++) {
-    if (!answer.length || answer[answer.length - 1] < nums[i])
+  const upper_bound = (target) => {
+    let start = 0;
+    let end = answer.length - 1;
+    while (start < end) {
+      const mid = Math.floor((start + end) / 2);
+      if (answer[mid] < target) {
+        start = mid + 1;
+      } else {
+        end = mid;
+      }
+    }
+    return end;
+  };
+
+  let answer = [nums[0]];
+  for (let i = 1; i < nums.length; i++) {
+    if (answer[answer.length - 1] < nums[i]) {
       answer.push(nums[i]);
-    else answer[lowerBound(answer, nums[i])] = nums[i];
+    } else if (answer[answer.length - 1] > nums[i]) {
+      answer[upper_bound(nums[i])] = nums[i];
+    }
   }
   return answer.length;
 }
