6e662ffbfc440dc4e9944f11f760a0b549d5ef26
wonil <119827402+wonill@users.noreply.github.com>
2024-09-05 09:11:01 +0900

    [Silver III] Title: 수리공 항승, Time: 92 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+let [n, l] = input[0].split(" ").map(Number);
+let arr = input[1].split(" ").map(Number);
+arr.sort((a, b) => a - b);
+
+let cnt = 0;
+let i = 0;
+let start = 0;
+while (i < n) {
+  while (arr[i] - arr[start] < l) {
+    i++;
+  }
+  cnt++;
+  start = i;
+}
+console.log(cnt);
