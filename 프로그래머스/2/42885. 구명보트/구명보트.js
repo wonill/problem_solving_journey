e3ca0fb35447d3c041e2d2031360664faa99425c
wonil <119827402+wonill@users.noreply.github.com>
2024-09-08 20:29:58 +0900

    [level 2] Title: 구명보트, Time: 35.81 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,10 +1,8 @@
 function solution(people, limit) {
-    let answer = 0;
     people.sort((a, b) => a - b);
-    for (let i = 0, j = people.length; i <= --j;) {
+    for (var i = 0, j = people.length; i < --j;) {
       if (people[i] + people[j] <= limit) i++;
-      answer++;
     }
-    return answer;
+    return people.length - i;
   }
   
