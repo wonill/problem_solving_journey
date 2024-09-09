86ab516551e6a65f799c734b7267be64add251e8
wonil <119827402+wonill@users.noreply.github.com>
2024-09-09 15:22:12 +0900

    [level 3] Title: 기지국 설치, Time: 1.93 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/README.md
+++ 프로그래머스/3/12979. 기지국 설치/README.md
@@ -1,10 +1,10 @@
 # [level 3] 기지국 설치 - 12979 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12979#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12979) 
 
 ### 성능 요약
 
-메모리: 37.3 MB, 시간: 2.79 ms
+메모리: 36.3 MB, 시간: 1.93 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 06일 14:27:10
+2024년 09월 09일 15:22:06
 
 ### 문제 설명
 
--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.js
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.js
@@ -1,14 +1,11 @@
 function solution(n, stations, w) {
-  let answer = 0;
-
-  let signalless = [];
-  signalless.push(stations[0] - w - 1);
+  const range = 2 * w + 1;
+    
+  let signalless = [stations[0] - w - 1, n - stations[stations.length - 1] - w];
+    
   for (let i = 0; i < stations.length - 1; i++) {
-    signalless.push(stations[i + 1] - stations[i] - 2 * w - 1);
+    signalless.push(stations[i + 1] - stations[i] - range);
   }
-  signalless.push(n - stations[stations.length - 1] - w);
-  for (let i = 0; i < signalless.length; i++) {
-    if (signalless[i] > 0) answer += Math.ceil(signalless[i] / (2 * w + 1));
-  }
-  return answer;
+    
+  return signalless.reduce((a, c) => a += c > 0 ? Math.ceil(c / range) : 0, 0);
 }
