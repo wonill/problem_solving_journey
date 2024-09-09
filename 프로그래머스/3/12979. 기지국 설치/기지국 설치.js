369f80d1eeefa67b09569c746800d354c7ddca88
wonil <119827402+wonill@users.noreply.github.com>
2024-09-09 15:23:53 +0900

    [level 3] Title: 기지국 설치, Time: 1.79 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.js
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.js
@@ -1,11 +1,6 @@
 function solution(n, stations, w) {
   const range = 2 * w + 1;
-    
   let signalless = [stations[0] - w - 1, n - stations[stations.length - 1] - w];
-    
-  for (let i = 0; i < stations.length - 1; i++) {
-    signalless.push(stations[i + 1] - stations[i] - range);
-  }
-    
+  for (let i = 0; i < stations.length - 1; i++) signalless.push(stations[i + 1] - stations[i] - range);
   return signalless.reduce((a, c) => a += c > 0 ? Math.ceil(c / range) : 0, 0);
 }
