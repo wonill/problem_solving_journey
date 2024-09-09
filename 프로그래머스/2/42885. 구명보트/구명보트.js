909e0904772270b4b070e7fdd7c4bf83f3b3705e
wonil <119827402+wonill@users.noreply.github.com>
2024-09-09 09:58:05 +0900

    [level 2] Title: 구명보트, Time: 31.35 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,1 +1,1 @@
-const solution = (p, limit) => p.length - p.sort((a, b) => b - a).reduce((a, c, i) => a + (c + p[p.length-a-1] <= limit && i < p.length-a-1), 0);
+const solution=(p,limit)=>p.length-p.sort((a,b)=>b-a).reduce((a,c,i)=>a+(c+p[p.length-a-1]<=limit&&i<p.length-a-1),0);
