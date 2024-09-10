b2890403ebf0b5137137476542eeda6066c09c05
wonil <119827402+wonill@users.noreply.github.com>
2024-09-11 04:14:47 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.39 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,19 +1,18 @@
 function solution(board, moves) {
     let answer = 0;
     let stack = [];
+    // 행렬을 90도 돌려서 빈 공간 삭제
+    board = board.map((_, row) => board.map(col => col[row]).reverse()).map(v => v.filter(v => v != 0));
     moves.forEach(move => {
-        let pick;
-        board.some(row => {
-            if (row[move - 1]) {
-                pick = row[move - 1];
-                row[move - 1] = 0;
-                return true;
-            } 
-        })
+		    // move 위치에 해당하는 인형 뽑기
+        let pick = board[move - 1].pop();
+        // 뽑은게 없다면 continue (forEach의 return == for문의 continue)
         if (!pick) return;
+        // 뽑은 인형이 스택 맨 위에 있는거랑 같으면 스택에서 뽑고
         if (stack[stack.length - 1] === pick) {
             stack.pop();
             answer += 2;
+        // 다르면 추가
         } else stack.push(pick);
     })
     return answer;
