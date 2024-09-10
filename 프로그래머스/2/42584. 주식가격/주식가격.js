b0c279fd87c8ea5b16a6138a6ac1c7b7a35e0357
wonil <119827402+wonill@users.noreply.github.com>
2024-09-11 01:31:24 +0900

    [level 2] Title: 주식가격, Time: 33.77 ms, Memory: 42.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,53 +1,18 @@
-class Queue{
-    constructor(arr){
-        this.items = {};
-        for (let i = 0; i < arr.length; i++){
-            this.items[i] = arr[i];
-        }
-        this.headIndex = 0;
-        this.tailIndex = arr.length;
-    }
-    
-    enqueue(item){
-        this.items[this.tailIndex] = item;
-        this.tailIndex++;
-    }
-    
-    dequeue(){
-        if (this.tailIndex > this.headIndex){
-           let item = this.items[this.headIndex];
-           delete this.items[this.headIndex];
-           this.headIndex++;
-           return item;
-        }
-    }
-    
-    peek(){
-        return this.items[this.headIndex];
-    }
-    
-    getLength(){
-        return this.tailIndex - this.headIndex;
-    }
-    
-    get(i){
-        return this.items[this.headIndex + i];
-    }
-}
-
 function solution(prices) {
     let answer = [];
-    let queue = new Queue(prices);
-    let price;
-    while (price = queue.dequeue()){
-        let cnt = 0;
-        let len = queue.getLength();
-        for (let i = 0; i < len; i++){
-            cnt++;
-            if (price > queue.get(i)) break;
+    prices.reverse();
+    
+    let cnt;
+    while (price = prices.pop()){
+        cnt = 0;
+        for (let i = 0; i < prices.length; i++){
+            cnt = i + 1;
+            if (prices[prices.length - 1 - i] < price) break;
         }
         answer.push(cnt);
     }
     return answer;
 }
+
+
 
