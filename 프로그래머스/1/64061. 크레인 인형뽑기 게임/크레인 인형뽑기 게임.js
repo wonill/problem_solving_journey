281f2cae486c9bb904b7a6a618ec05893f4d8860
wonil <119827402+wonill@users.noreply.github.com>
2024-09-11 02:28:10 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.77 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -0,0 +1,20 @@
+function solution(board, moves) {
+    let answer = 0;
+    let stack = [];
+    moves.forEach(move => {
+        let pick;
+        board.some(row => {
+            if (row[move - 1]) {
+                pick = row[move - 1];
+                row[move - 1] = 0;
+                return true;
+            } 
+        })
+        if (!pick) return;
+        if (stack.length && stack[stack.length - 1] === pick) {
+            stack.pop();
+            answer += 2;
+        } else stack.push(pick);
+    })
+    return answer;
+}
