fe96e32bc50eeb0fe7ab2e86c071d4a011f165f3
wonil <119827402+wonill@users.noreply.github.com>
2024-09-15 02:27:32 +0900

    [Silver I] Title: 나이트의 이동, Time: 752 ms, Memory: 37984 KB -BaekjoonHub

---
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.js
@@ -0,0 +1,73 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n");
+
+let t = Number(input[0]);
+let answer = [];
+
+class Queue {
+  constructor() {
+    this.items = {};
+    this.headIndex = 0;
+    this.tailIndex = 0;
+  }
+
+  enqueue(item) {
+    this.items[this.tailIndex] = item;
+    this.tailIndex++;
+  }
+
+  dequeue() {
+    if (this.tailIndex > this.headIndex) {
+      const item = this.items[this.headIndex];
+      delete this.items[this.headIndex];
+      this.headIndex++;
+      return item;
+    }
+  }
+
+  getLength() {
+    return this.tailIndex - this.headIndex;
+  }
+}
+
+const dx = [-2, -1, 1, 2, -2, -1, 1, 2];
+const dy = [1, 2, 2, 1, -1, -2, -2, -1];
+
+const bfs = (l, start, target) => {
+  let [x, y] = start;
+  let [targetX, targetY] = target;
+
+  let visited = Array.from({ length: l }, () => Array(l).fill(0));
+  visited[x][y] = 1;
+  let queue = new Queue();
+  queue.enqueue(start);
+
+  while (queue.getLength()) {
+    let [curX, curY] = queue.dequeue();
+    if (curX === targetX && curY === targetY) return visited[curX][curY] - 1;
+
+    for (let i = 0; i < 8; i++) {
+      let [nx, ny] = [curX + dx[i], curY + dy[i]];
+      if (nx < 0 || ny < 0 || nx >= l || ny >= l) continue;
+      if (!visited[nx][ny]) {
+        visited[nx][ny] = visited[curX][curY] + 1;
+        queue.enqueue([nx, ny]);
+      }
+    }
+  }
+};
+let line = 1;
+while (t--) {
+  let l = Number(input[line]);
+  let start = input[line + 1].split(" ").map(Number);
+  let target = input[line + 2].split(" ").map(Number);
+
+  answer.push(bfs(l, start, target));
+  line += 3;
+}
+
+console.log(answer.join("\n"));
