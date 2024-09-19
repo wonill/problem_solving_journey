cc0a22b4bff82123d3afd19b9e428b118c98c549
wonil <119827402+wonill@users.noreply.github.com>
2024-09-19 20:36:51 +0900

    [level 3] Title: 베스트앨범, Time: 0.61 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,36 +1,21 @@
 function solution(genres, plays) {
     const genreMap = new Map();
-    const songMap = new Map();
-
-    // 장르별 재생 횟수와 노래 정보를 저장
-    for (let i = 0; i < genres.length; i++) {
-        const genre = genres[i];
-        const playCount = plays[i];
-
-        // 장르별 총 재생 횟수
-        genreMap.set(genre, (genreMap.get(genre) || 0) + playCount);
-
-        // 노래 정보
-        if (!songMap.has(genre)) songMap.set(genre, []);
-        songMap.get(genre).push({ index: i, plays: playCount });
+    let songInfo = [];
+    
+    for(let i = 0; i < genres.length; i++) {
+        genreMap.set(genres[i], (genreMap.get(genres[i]) || 0) + plays[i]);
+        songInfo.push({ index: i, genre: genres[i], plays: plays[i] });
     }
-
-    // 장르별 총 재생 횟수를 기준으로 정렬
-    const sortedGenres = [...genreMap.entries()].sort((a, b) => b[1] - a[1]);
-
+    
+    songInfo = songInfo.sort((a, b) => b.plays - a.plays);
+    const order = Array.from(genreMap.entries()).sort((a, b) => b[1] - a[1]);
     const answer = [];
-
-    // 각 장르별로 상위 2곡 선택
-    for (const [genre] of sortedGenres) {
-        const songs = songMap.get(genre);
-        // 노래를 재생 횟수 기준으로 내림차순 정렬
-        songs.sort((a, b) => b.plays - a.plays);
-        
-        // 상위 2곡의 인덱스 추가
-        for (let i = 0; i < Math.min(2, songs.length); i++) {
-            answer.push(songs[i].index);
-        }
+    for(const [a, _] of order) {
+        songInfo.filter(ele => ele.genre === a).forEach((ele, index) => {
+            if(index < 2) {
+                answer.push(ele.index);
+            }
+        })
     }
-
     return answer;
 }
