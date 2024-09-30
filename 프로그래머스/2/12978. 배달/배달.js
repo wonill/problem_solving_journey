f277b0bab61dcb35d9331eb37e0d64c83bdd9695
wonil <119827402+wonill@users.noreply.github.com>
2024-09-30 11:44:08 +0900

    [level 2] Title: 배달, Time: 6.92 ms, Memory: 37.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12978. 배달/배달.js
@@ -0,0 +1,17 @@
+function solution(N, road, K) {
+    let graph = Array.from({length : N + 1}, (_, i) => 
+                           Array.from({length : N + 1}, (_, j) => i === j ? 0 : 1e9));
+    
+    road.forEach(([a, b, c]) => {
+        graph[a][b] = Math.min(graph[a][b], c);
+        graph[b][a] = Math.min(graph[b][a], c);
+    });
+    for (let k = 1; k <= N; k++){
+        for (let a = 1; a <= N; a++){
+            for (let b = 1; b <= N; b++){
+                graph[a][b] = Math.min(graph[a][b], graph[a][k] + graph[k][b]);
+            }
+        }
+    }
+    return graph[1].reduce((a, c) => a + (c <= K),0);
+}
