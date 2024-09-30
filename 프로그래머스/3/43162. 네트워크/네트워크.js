f68409935508da2cf3a000951ee4201e45c48182
wonil <119827402+wonill@users.noreply.github.com>
2024-09-30 09:49:39 +0900

    [level 3] Title: 네트워크, Time: 0.81 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,21 +1,4 @@
 function solution(n, computers) {
     var answer = 0;
-    let visited = new Set();
-    
-    const dfs = (start) => {
-        visited.add(start);
-        
-        for (let i = 0; i < n; i++){
-            if (visited.has(i)) continue;
-            if (computers[start][i] === 1) dfs(i);
-        }
-    }
-    
-    for (let i = 0; i < n; i++){
-        if (visited.has(i)) continue;
-        dfs(i);
-        answer++;
-    }
-    
     return answer;
 }
