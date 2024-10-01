4ce79f79d1ce4b97267835d6f63727898f217322
wonil <119827402+wonill@users.noreply.github.com>
2024-10-01 16:21:31 +0900

    [level 3] Title: 외벽 점검, Time: 63.60 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/3/60062. 외벽 점검/외벽 점검.js
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.js
@@ -1,34 +1,46 @@
 function solution(n, weak, dist) {
-    const len = weak.length;
-    weak = weak.concat(weak.map(v => v + n));
-    
-    const permutations = getPermutations(dist);
-    
-    for (let i = 1; i <= dist.length; i++) {
-        for (let start = 0; start < len; start++) {
-            for (let perm of permutations) {
-                let count = 0;
-                let position = start;
-                
-                for (let j = 0; j < i; j++) {
-                    const end = weak[position] + perm[j];
-                    while (position < start + len && weak[position] <= end) {
-                        position++;
-                        count++;
-                    }
-                    if (count === len) return i;
+    const dfs = (index, target, count) => {
+        if(index >= weak.length - 1) {
+            if(answer === -1 || count < answer) {
+                answer = count;
+            }
+            return;
+        }
+        let nextIndex;
+        for(let i = index + 1; i < weak.length; i++) {
+            if(weak[i] > weak[index] + target) {
+                nextIndex = i;
+                break;
+            }
+            if(i === weak.length - 1) {
+                if(answer === -1 || count < answer) {
+                    answer = count;
                 }
+                return;
+            }
+        }
+        for(let i = 0; i < dist.length; i++) {
+            if(visited[i] === false) {
+                visited[i] = true;
+                dfs(nextIndex, dist[i], count + 1);
+                visited[i] = false;
             }
         }
     }
     
-    return -1;
-}
-
-function getPermutations(arr) {
-    if (arr.length <= 1) return [arr];
-    return arr.flatMap((v, i) => 
-        getPermutations(arr.slice(0, i).concat(arr.slice(i + 1)))
-            .map(p => [v].concat(p))
-    );
+    let answer = -1;
+    const visited = new Array(dist.length).fill(false);
+    
+    for(let i = 0; i < weak.length; i++) {
+        for(let j = 0; j < dist.length; j++) {
+            visited[j] = true;
+            dfs(0, dist[j], 1);
+            visited[j] = false;
+        }
+        
+        const temp = weak.shift();
+        weak.push(temp + n);
+    }
+    
+    return answer;
 }
