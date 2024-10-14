388d6a86170224bfeef87749ec92cfbf483730eb
wonil <119827402+wonill@users.noreply.github.com>
2024-10-14 16:32:29 +0900

    [level 3] Title: 사라지는 발판, Time: 30.87 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.js
+++ 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.js
@@ -1,32 +1,37 @@
 const EMPTY = 0;
 const AVAILABLE = 1;
 
-// 현재 플레이어 좌표 : r1, c1
-// 상대 플레이어 좌표 : r2, c2
-const play = (board, r1, c1, r2, c2) => {
-    if (board[r1][c1] == EMPTY) return [false, 0];
-    board[r1][c1] = EMPTY;
-    const row = board.length;
-    const col = board[0].length;
-    let min_win = Infinity;
-    let max_lose = 0;
-    for (let [dr, dc] of [[-1, 0], [1, 0], [0, -1], [0, 1]]) {
-        const [nr, nc] = [r1 + dr, c1 + dc];
-        if (nr >= 0 && nr < row && nc >= 0 && nc < col && board[nr][nc] == AVAILABLE) {
-            const [lose, count] = play(board, r2, c2, nr, nc);
-            if (lose) max_lose = Math.max(max_lose, count + 1);
-            else min_win = Math.min(min_win, count + 1);
-        }
-    }
-    board[r1][c1] = AVAILABLE;
-    if (min_win < Infinity) return [true, min_win];
-    else return [false, max_lose];
-}
+const dx = [1, -1, 0, 0];
+const dy = [0, 0, 1, -1];
 
 function solution(board, aloc, bloc) {
-    const [ar, ac] = aloc;
-    const [br, bc] = bloc;
-    const [_, count] = play(board, ar, ac, br, bc);
+    const row_len = board.length;
+    const col_len = board[0].length;
+    
+    const dfs = (board, x1, y1, x2, y2) => {
+        if (board[x1][y1] === EMPTY) return [false, 0];
+        
+        board[x1][y1] = EMPTY;
+        
+        let max_lose = 0;
+        let min_win = Infinity;
+        
+        for (let i = 0; i < 4; i++){
+            const nx = x1 + dx[i];
+            const ny = y1 + dy[i];
+            if (nx >= 0 && nx < row_len && ny >= 0 && ny < col_len && board[nx][ny] === AVAILABLE) {
+                const [lose, count] = dfs(board, x2, y2, nx, ny);
+                if (lose) max_lose = Math.max(max_lose, count + 1);
+                else min_win = Math.min(min_win, count + 1);
+            }
+        }
+        
+        board[x1][y1] = AVAILABLE;
+        
+        if (min_win < Infinity) return [true, min_win];
+        else return [false, max_lose];
+    }
+    
+    const [_, count] = dfs(board, aloc[0], aloc[1], bloc[0], bloc[1]);
     return count;
 }
-
