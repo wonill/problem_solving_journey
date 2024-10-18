544dd47cd3483f19720fa453d9e280c2eb8be6de
wonil <119827402+wonill@users.noreply.github.com>
2024-10-19 07:22:09 +0900

    [level 2] Title: 땅따먹기, Time: 52.07 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
@@ -1,9 +1,10 @@
 function solution(land) {
-    for (let i = 1; i < land.length; i++){
-        for (let j = 0; j < land[i].length; j++){
-            land[i][j] += Math.max(...land[i - 1].filter((_, idx) => j != idx));
-        }
+    for (let i = 1; i < land.length; i++) {
+        const prev = land[i - 1];
+        land[i][0] += Math.max(prev[1], prev[2], prev[3]);
+        land[i][1] += Math.max(prev[0], prev[2], prev[3]);
+        land[i][2] += Math.max(prev[1], prev[0], prev[3]);
+        land[i][3] += Math.max(prev[1], prev[2], prev[0]);
     }
-
     return Math.max(...land[land.length - 1]);
 }
