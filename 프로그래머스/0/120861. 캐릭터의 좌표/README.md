db66f6bd6278e659c714135dfbcca9e83997a5df
wonil <119827402+wonill@users.noreply.github.com>
2024-10-19 06:47:25 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/README.md
+++ 프로그래머스/0/120861. 캐릭터의 좌표/README.md
@@ -1,6 +1,6 @@
 # [level 0] 캐릭터의 좌표 - 120861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120861) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120861?language=javascript) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 01일 12:14:36
+2024년 10월 19일 06:47:23
 
 ### 문제 설명
 
--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,15 +1,17 @@
 const direction = {
-    left : [-1, 0],
-    right : [1, 0],
-    up : [0, 1],
-    down : [0, -1]
-}
+    left: [-1, 0],
+    right: [1, 0],
+    up: [0, 1],
+    down: [0, -1]
+};
+
 function solution(keyinput, board) {
-    return keyinput.reduce((a, c) =>{ 
-        let nextX = a[0] + direction[c][0];
-        let nextY = a[1] + direction[c][1];
-        return Math.abs(nextX) < (board[0] / 2) && Math.abs(nextY) < (board[1] / 2)
-        ? [nextX, nextY]
-        : [a[0], a[1]];
+    const [maxX, maxY] = board.map(v => Math.floor(v / 2));
+    return keyinput.reduce((pos, key) => {
+        const [dx, dy] = direction[key] || [0, 0];
+        return [
+            Math.max(-maxX, Math.min(maxX, pos[0] + dx)),
+            Math.max(-maxY, Math.min(maxY, pos[1] + dy))
+        ];
     }, [0, 0]);
 }
