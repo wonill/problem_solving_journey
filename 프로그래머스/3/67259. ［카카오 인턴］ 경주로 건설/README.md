a0ac7b9804329dced2043e66d475eb97364c9136
wonil <119827402+wonill@users.noreply.github.com>
2024-12-15 19:23:37 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 11.49 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/README.md
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/README.md
@@ -1,10 +1,10 @@
 # [level 3] [카카오 인턴] 경주로 건설 - 67259 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67259?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67259) 
 
 ### 성능 요약
 
-메모리: 39.7 MB, 시간: 47.69 ms
+메모리: 37.6 MB, 시간: 11.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 09일 23:57:18
+2024년 12월 15일 19:23:33
 
 ### 문제 설명
 
--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.js
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.js
@@ -1,91 +1,30 @@
 function solution(board) {
-    const heap = new MinHeap();
-    const n = board.length;
-    const costs = board.map(row => row.map(v => v === 1 ? [0, 0] : [Infinity, Infinity]));
-    const dx = [1, 0, 0, -1];
-    const dy = [0, 1, -1, 0];
+    const dx = [-1,0,1,0];
+    const dy = [0,1,0,-1];
     
-    // queue value => x, y, cost 총합, 이전 방향(0: row, 1: col);
-    heap.push({ x: 0, y: 0, cost: 0, prevDir: 0 });
-    heap.push({ x: 0, y: 0, cost: 0, prevDir: 1 });
+    const len = board.length;
+    // dp[x좌표][y좌표][들어온 방향]
+    const dp = Array.from({length: len}, () => Array.from({length: len}, () => new Array(4).fill(Infinity)));
     
-    while (!heap.isEmpty()) {
-        const { x: x, y: y, cost: cost, prevDir: dir } = heap.pop();
-        if (costs[x][y][dir] < cost) continue;
-        costs[x][y][dir] = cost;
-        
-        if (x === n - 1 && y === n - 1) continue;
-        
-        for (let i = 0; i < 4; i++) {
-            const nx = x + dx[i];
-            const ny = y + dy[i];
-            const nDir = Math.abs(dx[i]) === 1 ? 1 : 0;
-            const nCost = nDir === dir ? cost + 100 : cost + 600;
-            if (nx > -1 && ny > -1 && nx < n && ny < n && costs[nx][ny][nDir] >= nCost) {
-                heap.push({x: nx, y: ny, cost: nCost, prevDir: nDir});
+    // 시작점에서 오른쪽으로 가는 경우와 시작점에서 아래로 가는 경우로 시작
+    const q = [[0, 0, 1, 0], [0, 0, 2, 0]];
+    dp[0][0][1] = 0;
+    dp[0][0][2] = 0;
+                          
+    while(q.length) {
+        const target = q.shift();
+        for(let i = 0; i < 4; i++) {
+            const nx = target[0] + dx[i];
+            const ny = target[1] + dy[i];
+            if(nx >= 0 && nx < len && ny >= 0 && ny < len && board[nx][ny] === 0) {
+                const newCost = target[2] === i ? target[3] + 100 : target[3] + 600;
+                if(newCost < dp[nx][ny][i]) {
+                    dp[nx][ny][i] = newCost;
+                    q.push([nx, ny, i, newCost]);
+                }
             }
         }
     }
     
-    return Math.min(...costs[n - 1][n - 1]);
-}
-
-class MinHeap {
-    constructor() {
-        this.heap = [null];
-    }
-    
-    push(val) {
-        this.heap.push(val);
-        let currentIndex = this.heap.length - 1;
-        let parentIndex = Math.floor(currentIndex / 2);
-        
-        while (parentIndex !== 0 && this.heap[currentIndex].cost < this.heap[parentIndex].cost) {
-            this._swap(currentIndex, parentIndex);
-            currentIndex = parentIndex;
-            parentIndex = Math.floor(currentIndex / 2);
-        }
-    }
-    
-    pop() {
-        if (this.isEmpty()) return;
-        if (this.heap.length === 2) return this.heap.pop();
-        
-        const val = this.heap[1];
-        this.heap[1] = this.heap.pop();
-        
-        let currentIndex = 1;
-        let leftIndex = 2;
-        let rightIndex = 3;
-        
-        while (
-            this.heap[leftIndex] && this.heap[currentIndex].cost > this.heap[leftIndex].cost ||
-            this.heap[rightIndex] && this.heap[currentIndex].cost > this.heap[rightIndex].cost 
-        ) {
-            if (this.heap[leftIndex] === undefined) {
-                this._swap(rightIndex, currentIndex);
-            } else if (this.heap[rightIndex] === undefined) {
-                this._swap(leftIndex, currentIndex);
-            } else if (this.heap[leftIndex].cost > this.heap[rightIndex].cost) {
-                this._swap(currentIndex, rightIndex);
-                currentIndex = rightIndex;
-            } else if (this.heap[leftIndex].cost <= this.heap[rightIndex].cost) {
-                this._swap(currentIndex, leftIndex);
-                currentIndex = leftIndex;
-            }
-            
-            leftIndex = currentIndex * 2;
-            rightIndex = currentIndex * 2 + 1;
-        }
-        
-        return val;
-    }
-    
-    isEmpty() {
-        return this.heap.length === 1;
-    }
-    
-    _swap(a, b) {
-        [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
-    }
+    return Math.min(...dp[len - 1][len - 1]);
 }
