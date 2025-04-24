77ef02c4fd8d4baf0845655b7b6553d7532a865f
wonil <119827402+wonill@users.noreply.github.com>
2025-04-25 06:53:02 +0900

    [level 1] Title: 유연근무제, Time: 1.21 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/README.md
+++ 프로그래머스/1/388351. 유연근무제/README.md
@@ -1,10 +1,10 @@
 # [level 1] 유연근무제 - 388351 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388351) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388351#) 
 
 ### 성능 요약
 
-메모리: 37 MB, 시간: 1.75 ms
+메모리: 33.6 MB, 시간: 1.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 24일 00:00:13
+2025년 04월 25일 06:53:00
 
 ### 문제 설명
 
--- 프로그래머스/1/388351. 유연근무제/유연근무제.js
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.js
@@ -1,22 +1,18 @@
+function isHourChanged(time){
+    return time % 100 >= 60;
+}
+
 function solution(schedules, timelogs, startday) {
     let result = 0;
-    schedules.forEach((e,i)=> {
-        const hour = Math.floor(e / 100);
-        const minute = e % 100;
-        const time_value = (hour * 60) + minute;
-        let flag = true;
-        for(let j=0; j<7; j++){
-            const now_day = (startday + j) % 7;
-            const now_value = Math.floor(timelogs[i][j]/100) * 60 + timelogs[i][j] % 100;
-            if(now_day === 0 || now_day === 6) {
-                continue;
-            }
-            if(now_value > time_value + 10) {
-                flag = false;
-                break;
-            }
-        }
-        if(flag) result++;
-    })
+    
+    const arrivalDeadline = schedules.map(time => time + (isHourChanged(time + 10) ? 50 : 10));
+    
+    const saturdayIndex = 6 - (startday % 7);
+    const sundayIndex = (saturdayIndex + 1) % 7;
+     
+    timelogs.forEach((timelog, index) => result += timelog.every((time, idx) => idx === saturdayIndex ||
+                                                                 idx === sundayIndex ||
+                                                                 time <= arrivalDeadline[index]) ? 1 : 0)
+    
     return result;
 }
