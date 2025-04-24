2b4065efc8a124c6f81e778cf48744c9a593680f
wonil <119827402+wonill@users.noreply.github.com>
2025-04-25 05:37:48 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
@@ -1,9 +1,10 @@
 function solution(n, w, num) {
-    let answer = 0;
     let count = 0;
     let row = 0;
-    const boxes = Array.from({length: Math.ceil(n / w)}, (_, i) => row++ % 2 === 0 ? Array.from({length: w}, () => ++count) : Array.from({length: w}, () => ++count).reverse()
-    );
+    const boxes = Array.from({length: Math.ceil(n / w)}, (_, i) => {
+      const rowArr = Array.from({length: w}, () => ++count);
+      return row++ % 2 === 0 ? rowArr : rowArr.reverse(); 
+    });
     const targetRow = Math.ceil(num / w) - 1;
     const targetCol = boxes[targetRow].findIndex((v) => v === num)
     const isEmpty = boxes.at(-1)[targetCol] > n;
