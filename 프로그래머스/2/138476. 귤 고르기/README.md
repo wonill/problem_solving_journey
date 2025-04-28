2a57da012fa7d98b0f8634fe41604b7de6698090
wonil <119827402+wonill@users.noreply.github.com>
2025-04-28 23:58:54 +0900

    [level 2] Title: 귤 고르기, Time: 23.78 ms, Memory: 47.3 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/README.md
+++ 프로그래머스/2/138476. 귤 고르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 귤 고르기 - 138476 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476) 
 
 ### 성능 요약
 
-메모리: 47 MB, 시간: 55.47 ms
+메모리: 47.3 MB, 시간: 23.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 22일 17:13:25
+2025년 04월 28일 23:58:51
 
 ### 문제 설명
 
--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,26 +1,10 @@
 function solution(k, tangerine) {
-    var answer = 0;
-    let num_of_tangerine = [];
-    tangerine.sort((a, b) => a - b);
-    let count = 0;
-    let now;
-    for (let i = 0; i < tangerine.length; i++){
-        if (i === 0) now = tangerine[i];
-        if (tangerine[i] != now){
-            num_of_tangerine.push([now, count]);
-            now = tangerine[i];
-            count = 0;
-        }
-        count++;
-    }
-    num_of_tangerine.push([now, count]);
-    num_of_tangerine.sort((a, b)=>b[1] - a[1]);
-    let sum = 0;
-    count = 1;
-    for (let i = 0; i < num_of_tangerine.length; i++){
-        if (sum + num_of_tangerine[i][1] >= k) return count;
-        sum += num_of_tangerine[i][1];
-        count++;
-    }
-    return answer;
+    let map = new Map();
+    tangerine.forEach((v) => {
+        map.set(v, (map.get(v) || 0) + 1);
+    })
+    let counts = Array.from(map.values()).sort((a, b) => b - a);
+    for (var i = 0; k > 0; i++) k -= counts[i];
+    
+    return i;
 }
