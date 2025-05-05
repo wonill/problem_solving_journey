a84dd423b4ddc74e56c9e3c00d1c533d292c175f
wonil <119827402+wonill@users.noreply.github.com>
2025-05-05 23:59:47 +0900

    [level 2] Title: 피로도, Time: 4.98 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,19 +1,23 @@
+
 function solution(k, dungeons) {
-  let max = 0;
-  let visited = new Set();
-
-  const dfs = (x, k) => {
-    max = Math.max(visited.size, max);
-
-    for (let i = 0; i < dungeons.length; i++) {
-      if (!visited.has(i) && k >= dungeons[i][0]) {
-        visited.add(i);
-        dfs(x + 1, k - dungeons[i][1]);
-        visited.delete(i);
-      }
-      max = visited.size > max ? visited.size : max;
+		// 던전 개수가 적어서 백트래킹 안써도 될것 같아서 dfs 선에서 끝냄
+    function dfs(count, lef) {
+			  // 방문한 던전 개수 최고 기록
+        if(answer < count) answer = count;
+        
+        // 방문 가능 및 피로도 가능 검사
+        for(let i = 0; i < dungeons.length; i++) {
+            if(visited[i] == false && lef >= dungeons[i][0]) {
+                visited[i] = true;
+                dfs(count + 1, lef - dungeons[i][1]);
+                visited[i] = false;
+            }
+        }
     }
-  };
-  dfs(0, k);
-  return max;
+    
+    var answer = 0;
+    // 방문 여부 저장
+    const visited = new Array(dungeons.length).fill(false);
+    dfs(0, k);
+    return answer;
 }
